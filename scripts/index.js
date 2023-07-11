8a392469228d916f833f08fabe003e2de07b49d7
Lkaua22k <lkauan462@gmail.com>
2023-07-10 22:25:16 -0300

    Merge branch 'main' of https://github.com/LKaua22k/Focus_Timer_2.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,21 @@
+// Coutdows
 const play = document.querySelector('.play')
 const stop = document.querySelector('.stop')
 const add = document.querySelector('.more')
 const less = document.querySelector('.less')
 let minutesDisplay = document.querySelector('.minutes')
 let secondsDisplay = document.querySelector('.seconds')
-let Timeout
+let Timeout 
+
+// Button
+const forest = document.querySelector('.forest')
+const rain = document.querySelector('.rain')
+const coffe = document.querySelector('.coffeshop')
+const fire = document.querySelector('.fireplace')
+
+function remo(btn,color){
+    btn.style.backgroundColor = color
+}
 
 
 function countdown(){
@@ -36,17 +47,16 @@ function updateDisplay(minutes , seconds) {
 
 function moreMinutes(){
         let minutes = Number(minutesDisplay.textContent) + 5
-        seconds = 0
 
         if(minutes >= 100){
             minutes = 100
         }
 
-        updateDisplay(minutes, seconds)
+        updateDisplay(minutes, 0)
 }
 
 function lessMinutes(){
-        let minutes = Number(minutesDisplay.textContent) - 5
+    let minutes = Number(minutesDisplay.textContent) - 5
 
     if(minutes < 0){
         minutes = 0
@@ -74,3 +84,48 @@ less.addEventListener('click', function(){
     // alert("less")
     lessMinutes()
 })
+
+forest.addEventListener('click', () =>{
+    forest.classList.add('active')
+    rain.classList.remove('active')
+    coffe.classList.remove('active')
+    fire.classList.remove('active')
+    remo(forest, '#029D53')
+    rain.style.backgroundColor = '#E1E1E6'
+    coffe.style.backgroundColor = '#E1E1E6'
+    fire.style.backgroundColor = '#E1E1E6'
+} )
+
+rain.addEventListener('click', () =>{
+    forest.classList.remove('active')
+    rain.classList.add('active')
+    coffe.classList.remove('active')
+    fire.classList.remove('active')
+    remo(rain,'#02789D')
+    forest.style.backgroundColor = '#E1E1E6'
+    coffe.style.backgroundColor = '#E1E1E6'
+    fire.style.backgroundColor = '#E1E1E6'
+} )
+
+coffe.addEventListener('click', () =>{
+    forest.classList.remove('active')
+    rain.classList.remove('active')
+    coffe.classList.add('active')
+    fire.classList.remove('active')
+    remo(coffe,'#9D3A02')
+    forest.style.backgroundColor = '#E1E1E6'
+    rain.style.backgroundColor = '#E1E1E6'
+    fire.style.backgroundColor = '#E1E1E6'
+} )
+
+fire.addEventListener('click', () =>{
+    forest.classList.remove('active')
+    rain.classList.remove('active')
+    coffe.classList.remove('active')
+    fire.classList.add('active')
+    remo(fire,'#9D0B02')
+    rain.style.backgroundColor = '#E1E1E6'
+    coffe.style.backgroundColor = '#E1E1E6'
+    forest.style.backgroundColor = '#E1E1E6'
+} )
+
